dad5811794a463454ef83fb185869f6acb6fd483
Evgeniy Kolegov <jpriest1987@gmail.com>
2020-04-12 13:51:28 +0700

    добавление анимации ховеров, фокусов, скриптов на форму и т.д.

--- dist/js/main.js
+++ dist/js/main.js
@@ -80,3 +80,64 @@
 
   _toggler.addEventListener('click', _toggleMenu);
 })();
+
+// валидация формы
+
+(function () {
+  var KeyCode = {
+    BACKSPACE: 8,
+    ESCAPE: 27,
+    ENTER: 13
+  };
+
+  var cbSection = document.querySelector('.consult');
+
+  function validateCallbackForm(section) {
+    var form = section.querySelector('form');
+    var telInput = form.querySelector('input[type="tel"]');
+    var swap;
+
+
+    telInput.addEventListener('focus', function () {
+      telInput.value = swap || '+7(';
+    });
+
+    telInput.addEventListener('blur', function () {
+      telInput.value = '';
+    });
+
+    telInput.addEventListener('input', function () {
+      if (telInput.value.match(/\+$|\+7$|\+7\([^\d]/)) {
+        telInput.value = '+7('; // не допускает значений '+', '+7' и ввода после скобки нечисловых значений
+      }
+      if (!telInput.value.match(/\)/)) { // проверка на наличие закрывающей скобки
+        if (telInput.value.match(/\+7\(\d+/)) {
+          telInput.value = telInput.value.match(/\+7\(\d+/); // записывает в значение поля вводимые числа и блокирует ввод нечисловых значений
+        }
+
+        if (telInput.value.match(/\+7\(\d{3}/)) { // добавляет скобку после ввода 3 чисел
+          telInput.value += ')';
+        }
+      } else {
+        telInput.value = telInput.value.match(/\+7\(\d{3}\)\d{0,7}/);
+      }
+
+      telInput.addEventListener('keydown', function (evt) {
+        if (evt.keyCode === KeyCode.BACKSPACE && telInput.value.match(/\+7\(\d{3}\)$/)) { // добавляет возможность удалять закрывающую скобку и число перед ней
+          telInput.value = telInput.value.slice(0, -1);
+        }
+      });
+
+      telInput.addEventListener('change', function () {
+        swap = telInput.value;
+      });
+    });
+
+    telInput.addEventListener('invalid', function () {
+      telInput.setCustomValidity('Значение поля должно быть в формате: +7(999)9999999');
+    });
+
+  }
+
+  validateCallbackForm(cbSection);
+})();
